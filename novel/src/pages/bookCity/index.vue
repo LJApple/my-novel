78b97e10f4ef951e4713898c9e319b7416fba364
lvjun <lvjun@cocosurprise.com>
2018-11-21 12:06:06 +0800

    '1'

--- novel/src/pages/bookCity/index.vue
+++ novel/src/pages/bookCity/index.vue
@@ -1,44 +1,85 @@
 <template>
-  <div class="counter-warp">
+  <!-- <div class="counter-warp">
     <p>Vuex counter：{{ count }}</p>
     <p>
       <button @click="increment">+</button>
       <button @click="decrement">-</button>
     </p>
+  </div> -->
+  <div class="bt">
+    <div class="bt-title">猜你喜欢</div>
+    <div class="bt-content">
+      <div class="bt-list" v-for="item in bookList" :key="item.id" @click="navto">
+        <img class="btl-img" :src="item.imgUrl" mode="widthFix" alt="">
+        <div class="btl-desc">{{item.desc}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // Use Vuex
-import store from './store'
+// import store from './store'
 
 export default {
-  computed: {
-    count () {
-      return store.state.count
+  data () {
+    return {
+      bookList: [{
+        id: 1,
+        imgUrl: 'http://img4.imgtn.bdimg.com/it/u=3677854586,3916838952&fm=26&gp=0.jpg',
+        desc: '斗破苍穹'
+      },
+      {
+        id: 2,
+        imgUrl: 'http://img4.imgtn.bdimg.com/it/u=3677854586,3916838952&fm=26&gp=0.jpg',
+        desc: '斗破苍穹'
+      },
+      {
+        id: 3,
+        imgUrl: 'http://img4.imgtn.bdimg.com/it/u=3677854586,3916838952&fm=26&gp=0.jpg',
+        desc: '斗破苍穹'
+      },
+      {
+        id: 4,
+        imgUrl: 'http://img4.imgtn.bdimg.com/it/u=3677854586,3916838952&fm=26&gp=0.jpg',
+        desc: '斗破苍穹'
+      }]
     }
   },
+  computed: {
+    // count () {
+    //   return store.state.count
+    // }
+  },
   methods: {
-    increment () {
-      store.commit('increment')
-    },
-    decrement () {
-      store.commit('decrement')
+    navto () {
+      const url = '../bookBrowse/main'
+      wx.navigateTo({ url })
     }
   }
 }
 </script>
 
-<style>
-.counter-warp {
-  text-align: center;
-  margin-top: 100px;
-}
-.home {
-  display: inline-block;
-  margin: 100px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
-}
+<style lang="stylus" scoped rel="stylesheet/stylus">
+.bt
+  .bt-title
+    font-size 28rpx
+    color #333
+    font-weight bolder
+    padding-left 30rpx
+  .bt-content
+    display flex
+    width 100%
+    flex-wrap wrap
+    .bt-list
+      width 210rpx
+      margin 30rpx 0 0 30rpx
+      // background gray
+      .btl-img
+        width 210rpx
+      .btl-desc
+        font-size 24rpx
+        line-height 50rpx
+        color gray
+        text-align center
 </style>
--- novel/src/pages/bookshelf/index.vue
+++ novel/src/pages/bookshelf/index.vue
@@ -1,34 +1,82 @@
 <template>
-  <div>
-    <ul class="container log-list">
-      <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
-        <card :text="(index + 1) + ' . ' + log"></card>
-      </li>
-    </ul>
+  <!-- <div class="counter-warp">
+    <p>Vuex counter：{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+  </div> -->
+  <div class="bt">
+    <div class="bt-title">我的书架</div>
+    <div class="bt-content">
+      <div class="bt-list" v-for="item in bookList" :key="item.id" @click="navto">
+        <img class="btl-img" :src="item.imgUrl" mode="widthFix" alt="">
+        <div class="btl-desc">{{item.desc}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { formatTime } from '@/utils/index'
-import card from '@/components/card'
+// Use Vuex
+// import store from './store'
 
 export default {
-  components: {
-    card
-  },
-
   data () {
     return {
-      logs: []
+      bookList: [{
+        id: 1,
+        imgUrl: 'http://img4.imgtn.bdimg.com/it/u=3677854586,3916838952&fm=26&gp=0.jpg',
+        desc: '斗破苍穹'
+      },
+      {
+        id: 2,
+        imgUrl: 'http://img4.imgtn.bdimg.com/it/u=3677854586,3916838952&fm=26&gp=0.jpg',
+        desc: '斗破苍穹'
+      },
+      {
+        id: 3,
+        imgUrl: 'http://img4.imgtn.bdimg.com/it/u=3677854586,3916838952&fm=26&gp=0.jpg',
+        desc: '斗破苍穹'
+      },
+      {
+        id: 4,
+        imgUrl: 'http://img4.imgtn.bdimg.com/it/u=3677854586,3916838952&fm=26&gp=0.jpg',
+        desc: '斗破苍穹'
+      }]
     }
   },
-
-  created () {
-    const logs = (wx.getStorageSync('logs') || [])
-    this.logs = logs.map(log => formatTime(new Date(log)))
+  computed: {
+  },
+  methods: {
+    navto () {
+      const url = '../bookBrowse/main'
+      wx.navigateTo({ url })
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped rel="stylesheet/stylus">
+.bt
+  .bt-title
+    font-size 28rpx
+    color #333
+    font-weight bolder
+    padding-left 30rpx
+  .bt-content
+    display flex
+    width 100%
+    flex-wrap wrap
+    .bt-list
+      width 210rpx
+      margin 30rpx 0 0 30rpx
+      // background gray
+      .btl-img
+        width 210rpx
+      .btl-desc
+        font-size 24rpx
+        line-height 50rpx
+        color gray
+        text-align center
 </style>
